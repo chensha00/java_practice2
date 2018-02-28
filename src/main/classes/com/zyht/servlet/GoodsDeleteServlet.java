ebdd4a0ecc622825cf42b7fc51c9dd85a621d104
chendongaichengdu <36066825+chendongaichengdu@users.noreply.github.com>
2018-02-28 09:22:18 +0800

    下架
    商品下架修改

--- src/main/classes/com/zyht/servlet/GoodsDeleteServlet.java
+++ src/main/classes/com/zyht/servlet/GoodsDeleteServlet.java
@@ -13,22 +13,35 @@ import java.sql.SQLException;
 /**
  * Created by Administrator on 2018/2/27.
  */
-public class GoodsDeleteServlet extends HttpServlet {
 
+
+public class GoodsDeleteServlet extends HttpServlet {
+    /**
+     * @param request, response
+     * @Title: doGet
+     * @Description: 重写doGet方法
+     * @author chendong
+     * @date 2018/1/28
+     * @throw IOException, ServletException
+     */
     public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
         doPost(request,response);
     }
+    /**
+     * @param request, response
+     * @Title: doPost
+     * @Description: 重写doPost方法
+     * @author chendong
+     * @date 2018/1/28
+     * @throw IOException, ServletException
+     */
     public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=utf-8");
         GoodsSellerRelationService goodsSellerRelationService=new GoodsSellerRelationServiceImpl();
         String str = ""+request.getAttribute("sellerid");
         Long id = Long.parseLong(str);
-        try {
-            goodsSellerRelationService.deleteGoodsSellerRelationById(id);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        goodsSellerRelationService.deleteGoodsSellerRelationById(id);
         request.getRequestDispatcher("jsp/seller.jsp").forward(request, response);
     }
 }
