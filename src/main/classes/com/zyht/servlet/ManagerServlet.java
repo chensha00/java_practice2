d3563ba0e86894ca3ba1efcfebf1fe9aefac3929
holyshitthatguy <36251923+holyshitthatguy@users.noreply.github.com>
2018-03-06 11:12:34 +0800

    修改错误，添加shopCar代码

--- src/main/classes/com/zyht/servlet/ManagerServlet.java
+++ src/main/classes/com/zyht/servlet/ManagerServlet.java
@@ -35,32 +35,32 @@ public class ManagerServlet extends HttpServlet{
         AccountService accountService = (AccountService) context.getBean("accountService");
 //        冻结账户
         if(req.getParameter("frozen")!=null){
-            Account account = accountService.selectOne(Long.valueOf(req.getParameter("frozen")));
-            account.setIsFrozen(true);
-            accountService.modifyUser(account);
+//            Account account = accountService.selectOne(Long.valueOf(req.getParameter("frozen")));
+//            account.setIsFrozen(true);
+//            accountService.modifyUser(account);
         }
 //        解除冻结
         if(req.getParameter("not_frozen")!=null){
-            Account account = accountService.selectOne(Long.valueOf(req.getParameter("not_frozen")));
-            account.setIsFrozen(false);
-            accountService.modifyUser(account);
+//            Account account = accountService.selectOne(Long.valueOf(req.getParameter("not_frozen")));
+//            account.setIsFrozen(false);
+//            accountService.modifyUser(account);
         }
 //        注销账户
         if(req.getParameter("canceled")!=null){
-            Account account1 = accountService.selectOne(Long.valueOf(req.getParameter("canceled")));
-            account1.setIsCanceled(true);
-            accountService.modifyUser(account1);
+//            Account account1 = accountService.selectOne(Long.valueOf(req.getParameter("canceled")));
+//            account1.setIsCanceled(true);
+//            accountService.modifyUser(account1);
         }
 //        删除账户
         System.out.println(req.getParameter("delete"));
         if(req.getParameter("delete")!=null){
-            Account account2 = accountService.selectOne(Long.valueOf(req.getParameter("delete")));
-            account2.setIsDelete(true);
-            accountService.modifyUser(account2);
+//            Account account2 = accountService.selectOne(Long.valueOf(req.getParameter("delete")));
+//            account2.setIsDelete(true);
+//            accountService.modifyUser(account2);
         }
 
-        List<Account> list = accountService.selectAll();
-        req.setAttribute("accounts",list);
+//        List<Account> list = accountService.selectAll();
+//        req.setAttribute("accounts",list);
 
         req.getRequestDispatcher("/jsp/manager.jsp").forward(req,resp);
     }
