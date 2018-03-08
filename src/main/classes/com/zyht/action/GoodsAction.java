228bf7b829c23f4a88718a5634699a2f1609a209
fanhualuojinziguiti <36251853+fanhualuojinziguiti@users.noreply.github.com>
2018-03-08 15:35:09 +0800

    在Action中写入了数据传递语句

--- src/main/classes/com/zyht/action/GoodsAction.java
+++ src/main/classes/com/zyht/action/GoodsAction.java
@@ -1,4 +1,4 @@
-package com.zyht.action;/********************************************************************
+﻿package com.zyht.action;/********************************************************************
  /**
  * @Project: jsp_web
  * @Package classes.com.zyht.action
@@ -12,10 +12,12 @@ import com.opensymphony.xwork2.ActionSupport;
 import com.zyht.base.BaseAction;
 import com.zyht.domain.Goods;
 import com.zyht.service.GoodsService;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,6 +35,7 @@ import java.util.Map;
 public class GoodsAction extends ActionSupport implements BaseAction{
   public Long goodsId;
   public List<Goods> goodsList;
+    private HttpServletRequest request;
     ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
     GoodsService goodsService=(GoodsService)context.getBean("goodsService");
 
@@ -42,25 +45,29 @@ public class GoodsAction extends ActionSupport implements BaseAction{
      * @author guoxin
      * @date 2018-03-05
     */
-     public  String goodsShows(){
+    public  String goodsShows(){
+        request = ServletActionContext.getRequest();
+        Long [] ids=null;
         Map<String, Object> stringObjectMap = new HashMap<String, Object>();
-
-        goodsList=goodsService.queryGoodsByCondition(stringObjectMap);
-
+        if(ids!=null){
+            stringObjectMap.put("`GOODS_ID`",goodsId);
+        }
+        goodsList=goodsService.queryGoodsByCondition(stringObjectMap, 0, 5);
+        request.setAttribute("goodsAll",stringObjectMap);
         return RELATION;
     }
-
     /**
      * @ClassName goodsShow
      * @Description 查询单个商品
      * @author guoxin
      * @date 2018-03-07
     */
-
     public  String goodsShow(){
+        request = ServletActionContext.getRequest();
         Goods goods=new Goods();
         if(goodsId!=null)
             goods=goodsService.queryGoodsById(goodsId);
+        request.setAttribute("goods",goods);
         return RELATION;
     }
     /**
@@ -69,13 +76,11 @@ public class GoodsAction extends ActionSupport implements BaseAction{
      * @author guoxin
      * @date 2018-03-05
     */
-
    public String goodsAdd(){
        Goods goods =new Goods();
        int newGoods=goodsService.insertGoods(goods);
        return JUMP;
    }
-
     /**
      * @ClassName goodsUpdate
      * @Description 更新物品
@@ -91,7 +96,6 @@ public class GoodsAction extends ActionSupport implements BaseAction{
         int newGoods=goodsService.updateGoods(goods);
         return JUMP;
     }
-
     /**
      * @ClassName goodsDelete
      * @Description 删除物品
@@ -106,14 +110,12 @@ public class GoodsAction extends ActionSupport implements BaseAction{
         goodsService.deleteGoodsById(goodsId);
         return JUMP;
     }
-
     /**
      * @ClassName  goodsDeletes
      * @Description 批量删除
      * @author guoxin
      * @date 2018-03-06
     */
-
     public String goodsDeletes(){
         Long []ids=null ;
         Map<String, Object> stringObjectMap = new HashMap<String, Object>();
@@ -130,6 +132,14 @@ public class GoodsAction extends ActionSupport implements BaseAction{
 
     public void setGoodsId(Long goodsId) {
         goodsId = goodsId;
+    }
+
+    public HttpServletRequest getRequest() {
+        return request;
+    }
+
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
     }
 }
 
